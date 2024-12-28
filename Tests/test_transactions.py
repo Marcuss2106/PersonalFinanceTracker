283f3e9ae033d38c7919860793c86b396c7748cb
Marcuss2106 <marcuss2106@gmail.com>
2024-12-28 15:50:19 -0500

    refactor: test variables moved

--- Tests/test_transactions.py
+++ Tests/test_transactions.py
@@ -7,21 +7,17 @@ from datetime import datetime
 tm = TransactionManager()
 fh = FileHandler()
 
+category = "Groceries"
+type = "Expense"
+amount = Decimal("13.12")
+description = "Apples"
+date = datetime.now()
+
 def test_createTransaction():
-    category = "Groceries"
-    type = "Expense"
-    amount = Decimal("13.12")
-    description = "Apples"
-    date = datetime.now()
     transaction = Transaction(category, type, amount, description)
     assert str(transaction) == f"{date.isoformat(timespec="seconds")} Groceries Expense — $13.12, Apples"
 
 def test_writeTransaction():
-    category = "Groceries"
-    type = "Expense"
-    amount = Decimal("13.12")
-    description = "Apples"
-    date = datetime.now()
     transaction = Transaction(category, type, amount, description)
     tm.addTransaction(transaction, fh)
     assert fh.readRecentTransaction() == ({"_category": transaction.category,
