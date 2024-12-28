5a1ae0beea6dcb694dfe8a84679e8fadb63cc5ca
Marcuss2106 <marcuss2106@gmail.com>
2024-12-28 00:14:52 -0500

    datetime

--- Transaction.py
+++ Transaction.py
@@ -1,10 +1,12 @@
+import datetime
+
 class Transaction:
-    def __init__(self, category, type, description, amount, date):
+    def __init__(self, category, type, description, amount):
         self.category = category
         self.type = type # income or expense
         self.description = description
         self.amount = amount
-        self.date = date
+        self.date = datetime.datetime
     
     def __str__(self):
         return f"{self.date} {self.category} {self.type} — {self.amount}, {self.description} "
